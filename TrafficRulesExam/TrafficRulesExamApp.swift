f3b69c72aa0e0b285eaa0947043d2e0a10f7cd7a
Yaroslav <yaroslav.yashin@me.com>
2021-10-11 16:26:40 +0300

    Initial Commit

---
+++ TrafficRulesExam/TrafficRulesExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficRulesExamApp.swift
+//  TrafficRulesExam
+//
+//  Created by Yaroslav on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficRulesExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

e1bfb55a2a7bc4f2f96b70e0c9113ec5ca1e500b
Yaroslav <yaroslav.yashin@me.com>
2021-11-29 19:59:14 +0300

    Trying to fix github actions building issue.

--- TrafficRulesExam/Model/URLSession+async.swift
+++ TrafficRulesExam/Model/URLSession+async.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-extension URLSession {
-    @available(iOS, deprecated: 15.0, message: "This extension is no longer necessary. Use API built into SDK")
-    func data(from url: URL) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: url) { data, response, error in
-                guard let data = data, let response = response else {
-                    let error = error ?? URLError(.badServerResponse)
-                    return continuation.resume(throwing: error)
-                }
+// extension URLSession {
+//     @available(iOS, deprecated: 15.0, message: "This extension is no longer necessary. Use API built into SDK")
+//     func data(from url: URL) async throws -> (Data, URLResponse) {
+//         try await withCheckedThrowingContinuation { continuation in
+//             let task = self.dataTask(with: url) { data, response, error in
+//                 guard let data = data, let response = response else {
+//                     let error = error ?? URLError(.badServerResponse)
+//                     return continuation.resume(throwing: error)
+//                 }
 
-                continuation.resume(returning: (data, response))
-            }
+//                 continuation.resume(returning: (data, response))
+//             }
 
-            task.resume()
-        }
-    }
-}
+//             task.resume()
+//         }
+//     }
+// }
